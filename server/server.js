f0e79d266cffd5cdebc0a99a9d2d4bfa0224033d
kyledaviswx <kyledaviswx@gmail.com>
2022-11-06 14:30:03 -0600

    added new mutation methods. add friend, add reaction.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -9,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 const app = express();
 
